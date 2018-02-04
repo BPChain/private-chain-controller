eb3538cfcb4a9b3d0c79e52653dbe07ad66581c3
Finn K <finn-klessascheck@o2mail.de>
2018-02-04 16:41:09 +0100

    Fix some problems with env vars

--- private_chain_scripts/switchChainToFrom.sh
+++ private_chain_scripts/switchChainToFrom.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 # Switch the active chain to the one supplied via the first parameter
 
-let "chainToSwitchTo=$1"
-let "chainToSwitchFrom=$2"
+cd /home/"$USER"/bin/private-"$2"/Node || exit
+docker-compose down --remove-orphans
 
-cd /home/"$USER"/bin/private-"$chainToSwitchFrom"/Node || exit
-docker-compose down
-
-cd /home/"$USER"/bin/private-"$chainToSwitchTo"/Node || exit
+cd /home/"$USER"/bin/private-"$1"/Node || exit
 docker-compose up --build
