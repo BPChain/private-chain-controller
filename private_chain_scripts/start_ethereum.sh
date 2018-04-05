ae9d497703ed84f01f176091c6b71753bf8c59b8
Finn K <finn-klessascheck@o2mail.de>
2018-04-05 14:01:21 +0200

    Export hostname in start scripts

--- private_chain_scripts/start_ethereum.sh
+++ private_chain_scripts/start_ethereum.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Starts Ethereum blockchain
 
+export HOSTNAME
+
 cd /home/"$USER"/bin/private-ethereum/Node || exit
 docker-compose up --build
--- private_chain_scripts/start_xain.sh
+++ private_chain_scripts/start_xain.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Starts XAIN blockchain
 
+export HOSTNAME
+
 cd /home/"$USER"/bin/private-xain/Node || exit
 docker-compose up --build
