ed148d8a71e0ec8779d8ef5aefd63fd8f01679e6
Finn K <finn-klessascheck@o2mail.de>
2018-04-06 20:46:35 +0200

    inject username into path

--- service/initService-no-sudo.sh
+++ service/initService-no-sudo.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd /home/"$USER"/bin/private-chain-controller|| exit
+cd /home/"$USER"/bin/private-chain-controller/service || exit
 cp -v blockchainController.service /lib/systemd/system/blockchainController.service
 
 chmod 644 /lib/systemd/system/blockchainController.service
 systemctl daemon-reload
 systemctl enable blockchainController.service
-systemctl start blockchainController.service
+systemctl start blockchainController@$USER.service
--- service/initService.sh
+++ service/initService.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd /home/"$USER"/bin/private-chain-controller|| exit
+cd /home/"$USER"/bin/private-chain-controller/service || exit
 cp -v blockchainController.service /lib/systemd/system/blockchainController.service
 
 sudo chmod 644 /lib/systemd/system/blockchainController.service
 sudo systemctl daemon-reload
 sudo systemctl enable blockchainController.service
-sudo systemctl start blockchainController.service
+sudo systemctl start blockchainController@$USER.service
