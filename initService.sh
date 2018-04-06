fc54ea50d45f0ca74485b11963e4c695e88fb4c1
Finn K <finn-klessascheck@o2mail.de>
2018-04-06 13:56:15 +0200

    Add scripts to init service

--- initService.sh
+++ initService.sh
@@ -2,3 +2,8 @@
 
 cd /home/"$USER"/bin/private-chain-controller|| exit
 cp -v blockchainController.service /lib/systemd/system/blockchainController.service
+
+sudo chmod 644 /lib/systemd/system/blockchainController.service
+sudo systemctl daemon-reload
+sudo systemctl enable blockchainController.service
+sudo systemctl start blockchainController.service
