118222edb1ea08fae49ffafabaf355109e20e7dd
Anton von Weltzien <anton.weltzien@gmail.com>
2018-04-16 15:44:05 +0200

    Deployment now checks out dev

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,12 @@
 
 echo "Creating blockains directory"
 if [ ! -d "private-xain" ]; then
-	git clone https://github.com/BPChain/private-xain.git
+	git clone https://github.com/BPChain/private-xain.git -b dev
 fi
 if [ ! -d "private-multichain" ]; then
-	git clone https://github.com/BPChain/private-multichain.git
+	git clone https://github.com/BPChain/private-multichain.git -b dev
 fi
 if [ ! -d "private-ethereum" ]; then
-	git clone https://github.com/BPChain/private-ethereum.git
+	git clone https://github.com/BPChain/private-ethereum.git -b dev
 fi
 ./startController.sh
