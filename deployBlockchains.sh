a2c3aa2fadc08df9032f91df8be5e134ac31dde0
Anton von Weltzien <anton.weltzien@gmail.com>
2018-04-17 14:36:35 +0200

    Add pull if repo already cloned

--- deployBlockchains.sh
+++ deployBlockchains.sh
@@ -5,11 +5,24 @@ source virtualenv/bin/activate
 pip3 install -r requirements.txt 
 if [ ! -d "private-xain" ]; then
 	git clone https://github.com/BPChain/private-xain.git -b dev
+else
+    cd private-xain
+    git pull
+    cd ..
 fi
 if [ ! -d "private-multichain" ]; then
 	git clone https://github.com/BPChain/private-multichain.git -b dev
+else
+    cd private-multichain
+    git pull
+    cd ..
 fi
 if [ ! -d "private-ethereum" ]; then
 	git clone https://github.com/BPChain/private-ethereum.git -b dev
+else
+    cd private-ethereum
+    git pull
+    cd ..
 fi
+
 ./startController.sh
